2f0057a8bf34656ac26068e3c7c4f3d4478a3aad
gpshml <112215884+gpshml@users.noreply.github.com>
2026-02-06 14:44:50 +0000

    Update spd2010_touch.h

--- components/spd2010_touch/spd2010_touch.h
+++ components/spd2010_touch/spd2010_touch.h
@@ -45,7 +45,9 @@ struct TouchFrame {
   uint8_t touch_num{0};
 };
 
-class SPD2010Touch : public touchscreen::Touchscreen, public Component, public i2c::I2CDevice {
+class SPD2010Touch : public touchscreen::Touchscreen,
+                     public PollingComponent,
+                     public i2c::I2CDevice {
  public:
   void set_interrupt_pin(InternalGPIOPin *pin) { this->irq_pin_ = pin; }
   void set_polling_fallback_ms(uint16_t ms) { this->polling_fallback_ms_ = ms; }
@@ -82,4 +84,5 @@ class SPD2010Touch : public touchscreen::Touchscreen, public Component, public i
 
 }  // namespace spd2010_touch
 }  // namespace esphome
+
 
