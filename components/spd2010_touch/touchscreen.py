6569b33bc841c3d34d5f975d68775745b59971c7
gpshml <112215884+gpshml@users.noreply.github.com>
2026-02-07 15:20:58 +0000

    Update touchscreen.py

--- components/spd2010_touch/touchscreen.py
+++ components/spd2010_touch/touchscreen.py
@@ -1,18 +1,17 @@
 import esphome.codegen as cg
 import esphome.config_validation as cv
 from esphome import pins
+
 from esphome.components import i2c, touchscreen
-from esphome.const import CONF_ID, CONF_INTERRUPT_PIN, CONF_ADDRESS, CONF_RESET_PIN
-
-DEPENDENCIES = ["i2c"]
-AUTO_LOAD = ["touchscreen"]
-
+from esphome.components import pca9554  # <-- THIS was missing
+from esphome.const import CONF_ID, CONF_INTERRUPT_PIN, CONF_ADDRESS
 
 CONF_POLLING_FALLBACK_MS = "polling_fallback_ms"
 CONF_PCA9554_ID = "pca9554_id"
 CONF_RESET_IO = "reset_io"
 
-print("SPD2010_TOUCH: LOADED FROM GITHUB COMMIT <hash>")
+DEPENDENCIES = ["i2c", "pca9554"]
+AUTO_LOAD = ["touchscreen"]
 
 spd2010_ns = cg.esphome_ns.namespace("spd2010_touch")
 SPD2010Touch = spd2010_ns.class_(
@@ -25,12 +24,15 @@ CONFIG_SCHEMA = (
     touchscreen.TOUCHSCREEN_SCHEMA.extend(
         {
             cv.GenerateID(): cv.declare_id(SPD2010Touch),
+
             cv.Optional(CONF_INTERRUPT_PIN): pins.gpio_input_pin_schema,
             cv.Optional(CONF_ADDRESS, default=0x53): cv.hex_int,
-            # NEW: use expander pin for reset instead of a GPIOPin (avoids conflicts)
+
+            # NEW: use expander pin for reset instead of a GPIOPin (avoids pin conflicts)
             cv.Optional(CONF_PCA9554_ID): cv.use_id(pca9554.PCA9554),
             cv.Optional(CONF_RESET_IO): cv.int_range(min=0, max=7),
-            cv.Optional(CONF_POLLING_FALLBACK_MS, default=50): cv.int_range(min=10, max=1000),
+
+            cv.Optional(CONF_POLLING_FALLBACK_MS, default=50): cv.int_range(min=10, max=2000),
         }
     )
     .extend(i2c.i2c_device_schema(0x53))
@@ -48,16 +50,5 @@ async def to_code(config):
     if CONF_PCA9554_ID in config and CONF_RESET_IO in config:
         exp = await cg.get_variable(config[CONF_PCA9554_ID])
         cg.add(var.set_reset_expander(exp, config[CONF_RESET_IO]))
-        
+
     cg.add(var.set_polling_fallback_ms(config[CONF_POLLING_FALLBACK_MS]))
-
-
-
-
-
-
-
-
-
-
-
