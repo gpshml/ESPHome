1c5494bc6c344ab1618803596c5b6fb9b240b6e8
gpshml <112215884+gpshml@users.noreply.github.com>
2026-02-07 15:19:03 +0000

    Update touchscreen.py

--- components/spd2010_touch/touchscreen.py
+++ components/spd2010_touch/touchscreen.py
@@ -7,6 +7,11 @@ from esphome.const import CONF_ID, CONF_INTERRUPT_PIN, CONF_ADDRESS, CONF_RESET_
 DEPENDENCIES = ["i2c"]
 AUTO_LOAD = ["touchscreen"]
 
+
+CONF_POLLING_FALLBACK_MS = "polling_fallback_ms"
+CONF_PCA9554_ID = "pca9554_id"
+CONF_RESET_IO = "reset_io"
+
 print("SPD2010_TOUCH: LOADED FROM GITHUB COMMIT <hash>")
 
 spd2010_ns = cg.esphome_ns.namespace("spd2010_touch")
@@ -16,16 +21,15 @@ SPD2010Touch = spd2010_ns.class_(
     i2c.I2CDevice,
 )
 
-
-CONF_POLLING_FALLBACK_MS = "polling_fallback_ms"
-
 CONFIG_SCHEMA = (
     touchscreen.TOUCHSCREEN_SCHEMA.extend(
         {
             cv.GenerateID(): cv.declare_id(SPD2010Touch),
             cv.Optional(CONF_INTERRUPT_PIN): pins.gpio_input_pin_schema,
             cv.Optional(CONF_ADDRESS, default=0x53): cv.hex_int,
-            cv.Optional(CONF_RESET_PIN): pins.gpio_output_pin_schema,
+            # NEW: use expander pin for reset instead of a GPIOPin (avoids conflicts)
+            cv.Optional(CONF_PCA9554_ID): cv.use_id(pca9554.PCA9554),
+            cv.Optional(CONF_RESET_IO): cv.int_range(min=0, max=7),
             cv.Optional(CONF_POLLING_FALLBACK_MS, default=50): cv.int_range(min=10, max=1000),
         }
     )
@@ -41,11 +45,12 @@ async def to_code(config):
         irq = await cg.gpio_pin_expression(config[CONF_INTERRUPT_PIN])
         cg.add(var.set_interrupt_pin(irq))
 
-    if CONF_RESET_PIN in config:
-        rst = await cg.gpio_pin_expression(config[CONF_RESET_PIN])
-        cg.add(var.set_reset_pin(rst))
+    if CONF_PCA9554_ID in config and CONF_RESET_IO in config:
+        exp = await cg.get_variable(config[CONF_PCA9554_ID])
+        cg.add(var.set_reset_expander(exp, config[CONF_RESET_IO]))
         
     cg.add(var.set_polling_fallback_ms(config[CONF_POLLING_FALLBACK_MS]))
+
 
 
 
